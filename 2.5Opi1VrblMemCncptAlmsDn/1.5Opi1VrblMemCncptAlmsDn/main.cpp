133b1114ef70f37cd31128957223e34729f22367
Oppie1 <atindall729@gmail.com>
2026-07-18 16:41:06 -0500

    Add files via upload

---
+++ 2.5Opi1VrblMemCncptAlmsDn/1.5Opi1VrblMemCncptAlmsDn/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	//Declare int var tuna and initialize it to 99.
+	//CODE:
+
+	//Output statement that says tuna's value is before reassignment.
+	//CODE:
+
+	//Assign that variable to a new value '76' to demonstrate the assignment.
+	//CODE:
+
+	//Output statement that displays new tuna to screen.
+	//CODE:
+
+	return 0;
+}
