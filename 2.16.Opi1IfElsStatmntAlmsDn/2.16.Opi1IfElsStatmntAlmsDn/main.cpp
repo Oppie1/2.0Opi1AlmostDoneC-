7e0cd8f26cd9789341adfa267d3d464af64067ec
Oppie1 <atindall729@gmail.com>
2026-07-19 18:13:38 -0500

    Add files via upload

---
+++ 2.16.Opi1IfElsStatmntAlmsDn/2.16.Opi1IfElsStatmntAlmsDn/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+	//Declare int var named age initialized to 123
+	//CODE:
+
+	//Use if statement with condition of the var age being more than 137 without put in codeblock
+	//that says "You're older than 133".
+	//CODE:
+
+	//Use else if statement to test for an age over 100 and outputs declaration of that to the screen.
+	//CODE:
+
+	//Create another else if statement if the persons age is over 2 years old and a declaration/description of that.
+	//CODE:
+
+	//Use final else statement to catch remainder of possibilities to the people 1 or under.
+	//Output statement to describe what the else statement does.
+	//CODE:
+	
+	return 0;
+}
+
+
+
+//Key point: When a statement is true and execute none of the statements below it will execute.
