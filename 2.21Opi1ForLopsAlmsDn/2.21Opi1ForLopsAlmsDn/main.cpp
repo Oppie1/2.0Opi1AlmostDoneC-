481f25c1caf94eb4954ed2396033269656806acd
Oppie1 <atindall729@gmail.com>
2026-07-19 18:14:35 -0500

    Add files via upload

---
+++ 2.21Opi1ForLopsAlmsDn/2.21Opi1ForLopsAlmsDn/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+
+
+	cout << "Here we use a for loop that will print numbers (5-25). Then will go to next step in program." << endl;
+
+	//Use for loop that declares and initializes int x to 0 and uses less than in control to list numbers 0-25
+	//that increments by 5. Display output to screen during each iteration of loop.
+	//CODE:
+
+	cout << "The for loop has fully executed and you are at next step in main()" << endl;
+
+	//Out x to screen each iteration.
+	//CODE:
+
+	//Output message to notify user for loop fully executed and they are back in main.
+	//CODE:
+
+}
