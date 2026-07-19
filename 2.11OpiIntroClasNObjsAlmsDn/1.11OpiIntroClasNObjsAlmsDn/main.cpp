fca427655188692e0e5417d0b633dec7066a6457
Oppie1 <atindall729@gmail.com>
2026-07-18 22:52:29 -0500

    Add files via upload

---
+++ 2.11OpiIntroClasNObjsAlmsDn/1.11OpiIntroClasNObjsAlmsDn/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+//Declare a class named AdamClass
+//CODE:
+
+//Declare a public class access specifier.
+//CODE:
+
+//Declare string variable called name initialized to "Adam Tindall". REMEMBER we need semi colon (;) at end of class braces };
+//CODE:
+
+//Declare void function with no parameters named helloObject() that outputs welcome to AP intro to CS and name is...
+//CODE:
+
+int main() {
+
+	//Create/instantiate object1 and object2 -> generic names from Adam Class
+	//CODE:
+
+	//Use object1 to call function we created outside of main. Use object2 to 
+	//to call the name variable. Then output to screen at same time using cout
+	//to produce a complete sentence.
+	//CODE:
+
+	cout << "\n-----------Bonus----------\n" << endl;
+	cout << "You can create an object that  does two things at once:" << endl;
+
+	//Use object (here object3) that can hold two pieces of information that can be used. 
+	//Create/instantiate object, use SAME object to call helloObject() and outputs name to screen.
+	//CODE:
+
+	return 0;
+
+}
---
+++ 2.12Opi1VarsInClsesAlmsDn/2.12Opi1VarsInClsesAlmsDn/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+//Create a class named codeRocks.
+//CODE:
+
+//Create a public access specifier:
+//CODE:
+
+//Declare void setter setName() that takes string parameter x. Then set value x equal to var that will 
+//store information from main. Make it descriptive like "welcomeStatement".
+//CODE:
+
+//Declare getName() of data type string with no parameters that will return stored information in 
+//"welcomeStatement" to caller (main() here). 
+//CODE:
+
+//Declare private access specifier.
+//CODE:
+
+//Create the string variable ("welcomeStatement") that will store sentence/string input from main.
+//CODE:
+
+
+int main() {
+
+	//Create an object(object1) of the codeRocks.
+	//CODE:
+	
+	//Call on object1 and string/sentence of your choosing using the setName() within in argument
+	//which will store in the welcomeStatement variable.
+	//CODE:
+
+	//Output statement and object1 you created to retrieve the sentence stored in string.
+	//CODE:
+
+}
