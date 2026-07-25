eb879ede91f1a1e3bdb4ec884057a1d296f79b83
Oppie1 <atindall729@gmail.com>
2026-07-24 19:15:58 -0500

    Add files via upload

---
+++ 2.55Opi1VrtulFuncsAlmsDn/2.55Opi1VrtulFuncsAlmsDn/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+//Declare Enemy class
+//CODE:
+
+//Declare public access specifier.
+//CODE:
+
+//Declare virtual void attack() function that outputs this default attack when none given in class.
+//CODE:
+
+//Declare derived class named Ninja of the Base class Enemy
+//CODE:
+
+//Declare public access specifier.
+//CODE:
+
+//***Here we comment out the Ninja attack function and the Enemy class member function
+// takes over***
+
+//***If you uncomment out the Ninja attack() function the attack will overwrite the 
+//virtual base class member function and just print the Ninja attack() function to screen.
+
+//void attack() {
+//cout << "Ninja attack! " << endl;
+
+//Declare Monster derived class of the Base class Enemy
+//CODE:
+
+//Declare public access specifier.
+//CODE:
+
+//Declare monster void attack function that prints I am a monster attack to the screen
+//CODE:
+
+
+int main() {
+
+	//Declare n object of class Ninja.
+	//CODE:
+
+	//Declare m object of class Monster
+	//CODE:
+
+	//Declare pointer named enemy1 of class Enemy that holds memory address of n.
+	//CODE:
+
+	//Declare pointer named enemy2 of class Enemy that holds memory address of n
+	//CODE:
+
+	//Use enemy1 pointer with arrow member selection operator to call attack() function.
+	//CODE:
+
+	//Use enemy2 pointer with the arrow member selection operator to call attack() function.
+
+}
