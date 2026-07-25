5178213f6a109a4e95c4d58a06ce0031de12c4d3
Oppie1 <atindall729@gmail.com>
2026-07-25 17:57:28 -0500

    Add files via upload

---
+++ PumpBrksAlmsDn/2.58OpiFncWMultiPrmtrsPmpBrks/2.58OpiFncWMultiPrmtrsPmpBrks/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+//Declare template for the FIRST and SECOND class.
+//CODE:
+
+//Declare smaller() that returns type FIRST, taking parameter a of FIRST and parameter b of type SECOND.
+//CODE:
+
+//Make return statement that says a is less than b return a and else return b. Use ternary operator.
+//CODE:
+
+
+int main() {
+
+	//Declare int x initialized to 89.
+	//CODE:
+
+	//Declare a double y and initialize it to 56.78.
+	//CODE:
+
+	//Output to screen function call smaller() with x and y as parameters
+	//CODE:
+
+}
