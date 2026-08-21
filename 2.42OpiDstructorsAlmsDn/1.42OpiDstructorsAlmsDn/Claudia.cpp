b8e30bf8337b208a76313c1ceba87e8418f97d19
Oppie1 <atindall729@gmail.com>
2026-08-21 16:30:15 -0500

    Add files via upload

---
+++ 2.42OpiDstructorsAlmsDn/1.42OpiDstructorsAlmsDn/Claudia.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Claudia.h"
+using namespace std;
+
+
+
+//Define Claudia constructor. Output "this is the constructor which runs automatically when object is created"
+//CODE:
+
+//Define Claudia destructor, Output "This is the destructor. Runs automatically at very end of main() as objects are being destroyed. 
+//CODE:
---
+++ 2.42OpiDstructorsAlmsDn/1.42OpiDstructorsAlmsDn/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Claudia.h"
+using namespace std;
+
+
+
+int main() {
+
+
+	//Declare object of Claudia class.
+	//CODE:
+
+	//Output statement here in main() saying it runs after constructor but before destructor.
+	//CODE:
+
+}
