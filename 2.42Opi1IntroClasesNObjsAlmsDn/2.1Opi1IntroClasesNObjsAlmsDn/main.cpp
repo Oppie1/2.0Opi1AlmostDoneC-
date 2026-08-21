2a0b21197b23671ccca9759b133fc0de695e34d1
Oppie1 <atindall729@gmail.com>
2026-08-21 16:16:44 -0500

    Add files via upload

---
+++ 2.42Opi1IntroClasesNObjsAlmsDn/2.1Opi1IntroClasesNObjsAlmsDn/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+//Declare a class named codeRocksClass.
+//CODE:
+
+//Declare public access specifier.
+//CODE:
+
+//Declare string variable that stores a name.
+//CODE:
+
+//Declare int variable that will hold an age.
+//CODE:
+
+//Declare a variable that will store grade point average (decimal).
+//CODE:
+
+//Declare constructor with string, int and double as its parameter (a, b, c) which will set values to variables
+//where that information will be stored. The constructor MUST be class's name.
+//CODE:
+
+//Create void function that outputs Claude is studying when called.
+//CODE:
+
+//Create a car class
+//CODE:
+
+//Declare a public access specifier
+//CODE:
+
+//Declare variables (attributes) that will store information from constructor (year, make, model).
+//CODE:
+
+//Create a CarClass constructor with parameters and set those parameters equal to the variables you just made.
+//CODE:	
+
+//Create a void function that does something. Like prints out "Car hits the breaks" to the screen. 
+//CODE:
+
+int main() {
+
+	cout << "\nExample of using a 'public:' access specifier to demonstrate constructor which is doing two things at once:\n";
+	cout << "1. Create box/blank form. 2. put some default info in it!\n\n";
+
+
+	//Create a Oppie object of the codeRocksClass with parameters "Claude", 33, 3.7"
+	//CODE:
+
+	//Use that object to show name, age and gpa to screen
+	//CODE:
+
+	//Create an object and call the "study" function which will then print to screen.
+	//CODE:
+
+	//Instantiate another Oppie object named oppieObject2 of the Car class and perform the same steps.
+	//CODE:
+	
+}
