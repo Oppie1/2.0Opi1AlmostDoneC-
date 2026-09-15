88f835ba8716ef6356821f3a5fe8d59329c49c10
Oppie1 <atindall729@gmail.com>
2026-09-15 14:38:45 -0500

    Add files via upload

---
+++ PumpBrksAlmsDn/2.62OpiMorExcptnExmplPmpBrks/2.62OpiMorExcptnExmplPmpBrks/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+	//Use try keyword/block
+	//CODE:
+		cout << "Please enter a number: " << endl;
+
+		//Declare char variable f and initialize it to 0
+		//CODE:
+	
+		//Declare an uninitialized var named int answer.
+		//CODE:
+
+
+		//Declare an uninitialized int variable named num1.
+		//CODE
+
+		//Prompt user for first number with cout statement.
+		//CODE:
+
+		//Input user entry into num1 var
+		//CODE:
+	
+		//Declare an uninitialized int variable named num2.
+		//CODE:
+
+		//Prompt user for the second number.
+		//CODE:
+		cout << "Please enter a second number: " << endl;
+
+		//Input user input and store it in num2
+		//CODE:
+
+		//Use if statement with throw keyword to test if the second user input (num2) is 0
+		//CODE:
+
+		//Use throw with f as what the code block executes if true.
+		//CODE:
+
+		//Assign var answer to expression num1/num2 and output to the screen.
+		//CODE:
+
+	//Use catch all block ... with output one cannot divide by 0.
+	//CODE:
+
+}
